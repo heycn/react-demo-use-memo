6ce7c8a59f5c4c122bfc5e8992c3dc6c7ff170a5
heycn <x1chennan@qq.com>
2022-03-25 13:01:39 +0800

    使用 memo 能解决不必要的 render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { memo, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
@@ -19,11 +19,12 @@ const App = () => {
   )
 }
 
-const Child = props => {
+// 使用 memo 能解决不必要的 render
+const Child = memo(props => {
   console.log('执行了')
   console.log('如果这里有大量代码')
   return <div>Child: {props.data}</div>
-}
+})
 
 const root = document.getElementById('root')
 ReactDOM.render(<App />, root)
